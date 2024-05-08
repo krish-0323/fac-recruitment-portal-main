63676bd6cd48f9468ed698e728c1231e6b15156d
krish-0323 <krishnapurwar23022003@gmail.com>
2024-05-08 21:46:08 +0530

    update faculty_panel

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,34 @@ app.post('/signup', (req, res) => {
     });
 })
 
+app.post('/faculty_panel', (req, res) => {
+    const sql = "INSERT INTO personaldetails (`firstName`, `middleName`, `lastName`, `nationality`, `DOB`, `gender`, `martialStatus`, `ID_Proof`, `fathersName`, `correspondanceAddress`, `permanentAddress`, `email`, `altEmail`, `Mobile`, `altMobile`, `landlineNo`) VALUES (?)";
+    const values = [
+        req.body.firstName,
+        req.body.middleName,
+        req.body.lastName,
+        req.body.nationality,
+        req.body.DOB,
+        req.body.gender,
+        req.body.martialStatus,
+        req.body.ID_Proof,
+        req.body.fathersName,
+        req.body.correspondanceAddress,
+        req.body.permanentAddress,
+        req.body.email,
+        req.body.altEmail,
+        req.body.Mobile,
+        req.body.altMobile,
+        req.body.landlineNo
+    ]
+    db.query(sql, [values], (err, data) => {
+        if(err){
+            return res.json("Error");
+        }
+        return res.json(data);
+    });
+})
+
 app.post('/', (req, res) => {
     const sql = "SELECT * FROM login WHERE `email` = ? AND `password` = ?" ;
     db.query(sql, [req.body.email, req.body.password], (err, data) => {
--- frontend/src/Components/FacultyPanel/Facultypanel.jsx
+++ frontend/src/Components/FacultyPanel/Facultypanel.jsx
@@ -1,16 +1,55 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import Validation from './FCValidation';
+import axios from 'axios';
 const Facultypanel = () => {
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission here
-  };
 
   const [file, setFile] = useState();
   function handleChange(e) {
       console.log(e.target.files);
       setFile(URL.createObjectURL(e.target.files[0]));
+  }
+
+  const [values, setValues] = useState({
+    firstName: '',
+    middleName: '',
+    lastName: '',
+    nationality: '',
+    DOB: '',
+    Gender: '',
+    category: '',
+    martialStatus: '',
+    ID_Proof: '',
+    fathersName: '',
+    correspondenceAddress: '',
+    permanentAddress: '',
+    email: '',
+    altEmail: '',
+    Mobile: '',
+    altMobile: '',
+    landlineNo: ''
+  })
+
+  const [errors, setErrors] = useState({})
+  const handleInput = (event) => {
+    setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+    console.log(event.target.value);
+  }
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(Validation(values));
+    const err = Validation(values);
+    axios.post('http://localhost:8081/faculty_panel', values)
+    .then(res => {
+        navigate('/academic');
+      }
+    )
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   return (
@@ -60,6 +99,7 @@ const Facultypanel = () => {
           </div>
 
           <div className='w-screen'>
+            <form action='' onSubmit={handleSubmit}>
           
               {/* First Column */}
               <div className='grid grid-cols-2 gap-y-4 mb-4 justify-center px-2 py-5 rounded-md' style={{backgroundColor:"#8ecae6"}}>
@@ -74,12 +114,14 @@ const Facultypanel = () => {
 
                 <div className='grid grid-cols-2'>
                   <div><label>Application Number:</label></div>
-                  <div><input type="text" className='w-5/6 text-sm p-1' required /></div>
+                  <div><input type="text" className='w-5/6 text-sm p-1' required
+                  onChange={handleInput} /></div>
                 </div>
 
                 <div className='grid grid-cols-2'>
                   <div><label>Date of Application:</label></div>
-                  <div><input type="date" className='w-5/6 text-sm p-1' required /></div>
+                  <div><input type="date" className='w-5/6 text-sm p-1' required 
+                  onChange={handleInput}/></div>
                 </div>
 
                 <div className='grid grid-cols-2'>
@@ -119,30 +161,36 @@ const Facultypanel = () => {
                   <div className='grid grid-cols-2 w-3/4 gap-4'>
                     <div className='grid grid-cols-2'>
                       <div><label>First Name*:</label></div>
-                      <div><input type="text" className='w-5/6' required /></div>
+                      <div><input type="text" className='w-5/6' 
+                      name='firstName' onChange={handleInput} required /></div>
                     </div>
                     <div className='grid grid-cols-2'>
                       <div><label>Middle Name:</label></div>
-                      <div><input type="text" className='w-5/6' /></div>
+                      <div><input type="text"
+                      name='middleName' onChange={handleInput} className='w-5/6' /></div>
                     </div>
                     <div className='grid grid-cols-2'>
                       <div><label>Last Name*:</label></div>
-                      <div><input type="text" className='w-5/6' required /></div>
+                      <div><input type="text" className='w-5/6'
+                      name='lastName' onChange={handleInput} required /></div>
                     </div>
                     <div className='grid grid-cols-2'>
                       <div><label>Nationality*:</label></div>
-                      <select className='w-5/6' required>
+                      <select className='w-5/6'
+                      name='nationality' onChange={handleInput} required>
                         <option value="Indian">Indian</option>
                         {/* Add other nationality options here */}
                       </select>
                     </div>
                     <div className='grid grid-cols-2'>
                       <div><label>Date of Birth*:</label></div>
-                      <div><input type="date" className='w-5/6' required /></div>
+                      <div><input type="date" className='w-5/6'
+                      name='DOB' onChange={handleInput} required /></div>
                     </div>
                     <div className='grid grid-cols-2'>
                       <div><label>Gender*:</label></div>
-                      <div><select className='w-5/6' required>
+                      <div><select className='w-5/6'
+                      name='gender' onChange={handleInput} required>
                         <option value="M">Male</option>
                         <option value="F">Female</option>
                         <option value="Other">Other</option>
@@ -152,7 +200,8 @@ const Facultypanel = () => {
                     <div className='grid grid-cols-2'>
                       <div><label>Marital Status*:</label></div>
                       <div>
-                        <select className='w-5/6' required>
+                        <select className='w-5/6'
+                        name='martialStatus' onChange={handleInput} required>
                           <option value="Married">Married</option>
                           <option value="Unmarried">Unmarried</option>
                           <option value="Other">Other</option>
@@ -161,13 +210,14 @@ const Facultypanel = () => {
                     </div>
                     <div className='grid grid-cols-2'>
                       <div><label>Category*:</label></div>
-                      <div><input type="text" className='w-5/6' required /></div>
+                      <div><input type="text" className='w-5/6'
+                      name='category' onChange={handleInput} required /></div>
                     </div>
                     <div className='grid grid-cols-2'>
                       <div><label>ID Proof*:</label></div>
                       <div>
-                      <select className='w-5/6' required>
-                        <option value="Select">Select</option>  
+                      <select className='w-5/6'
+                      name='ID_Proof' onChange={handleInput} required>
                         <option value="AADHAR">AADHAR</option>  
                         <option value="Pan Card">Pan Card</option>  
                         <option value="Driving License">Driving License</option>  
@@ -180,18 +230,20 @@ const Facultypanel = () => {
                     </div>
                     <div className='grid grid-cols-2'>
                       <div><label>Father's Name*:</label></div>
-                      <div><input type="text" className='w-5/6' required /></div>
+                      <div><input type="text" className='w-5/6'
+                      name='fathersName' onChange={handleInput} required /></div>
                     </div>
                     <div className='grid grid-cols-2'>
                       <div><label>ID Proof File*:</label></div>
-                      <div><input type="file" className='w-5/6' required /></div>
+                      <div><input type="file" className='w-5/6'
+                      name='' onChange={handleInput} required /></div>
                     </div>
                   </div>
 
                   <div className='w-1/4 h-20 text-sm'>
                   <div className="App">
                     <h2>Upload Profile:</h2>
-                    <input type="file" onChange={handleChange} />
+                    <input type="file" onChange={handleChange} required/>
                     <img src={file} />
                   </div>
                   </div>
@@ -202,46 +254,16 @@ const Facultypanel = () => {
                 <div className='flex flex-wrap gap-6'>
                   <div className='w-full'><h2>Correspondance Address</h2></div>
                   <div className='w-full'>
-                    <textarea rows="2" cols="50" className='rounded-md'>
-                    </textarea>
-                  </div>
-                  <div className='w-full'>
-                    <textarea rows="2" cols="50" className='rounded-md'>
-                    </textarea>
-                  </div>
-                  <div className='w-full'>
-                    <textarea rows="2" cols="50"  className='rounded-md'>
-                    </textarea>
-                  </div>
-                  <div className='w-full'>
-                    <textarea rows="2" cols="50" className='rounded-md'>
-                    </textarea>
-                  </div>
-                  <div className='w-full'>
-                    <textarea rows="2" cols="50" className='rounded-md'>
+                    <textarea rows="2" cols="50"
+                    name='correspondanceAddress' onChange={handleInput} className='rounded-md'>
                     </textarea>
                   </div>
                 </div>
                 <div className='flex flex-wrap gap-6'>
                   <div><h2>Permanent Address</h2></div>
                   <div className='w-full'>
-                    <textarea rows="2" cols="50" className='rounded-md'>
-                    </textarea>
-                  </div>
-                  <div className='w-full'>
-                    <textarea rows="2" cols="50" className='rounded-md'>
-                    </textarea>
-                  </div>
-                  <div className='w-full'>
-                    <textarea rows="2" cols="50" className='rounded-md'>
-                    </textarea>
-                  </div>
-                  <div className='w-full'>
-                    <textarea rows="2" cols="50" className='rounded-md'>
-                    </textarea>
-                  </div>
-                  <div className='w-full'>
-                    <textarea rows="2" cols="50" className='rounded-md'>
+                    <textarea rows="2" cols="50"
+                    name='permanentAddress' onChange={handleInput} className='rounded-md'>
                     </textarea>
                   </div>
                 </div>
@@ -250,41 +272,49 @@ const Facultypanel = () => {
               <div className='grid grid-cols-2 gap-x-20 gap-y-4 my-4 justify-center px-2 py-5 rounded-md' style={{backgroundColor:"#8ecae6"}}>
                 <div className='grid grid-cols-2'>
                   <div><label>Mobile Number:</label></div>
-                  <div><input type="text" className='w-5/6' required /></div>
+                  <div><input type="text" className='w-5/6'
+                  name='Mobile' onChange={handleInput} required /></div>
                 </div>
 
                 <div className='grid grid-cols-2'>
                   <div><label>Email:</label></div>
-                  <div><input type="email" className='w-5/6' required /></div>
+                  <div><input type="email" className='w-5/6'
+                  name='email' onChange={handleInput} required /></div>
                 </div>
 
                 <div className='grid grid-cols-2'>
                   <div><label>Alternate Mobile Number:</label></div>
-                  <div><input type='BigInt' className='w-5/6' required /></div>
+                  <div><input type='BigInt' className='w-5/6'
+                  name='altMobile' onChange={handleInput} required /></div>
                 </div>
 
                 <div className='grid grid-cols-2'>
                   <div><label>Alternate Email:</label></div>
                   <div>
-                    <input type="email" className='w-5/6'></input>
+                    <input type="email"
+                    name='altEmail' onChange={handleInput} className='w-5/6'></input>
                   </div>
                 </div>
 
                 <div className='grid grid-cols-2'>
                   <div><label>Landline No:</label></div>
-                  <div><input type="BigInt" className='w-5/6' required /></div>
+                  <div><input type="BigInt"
+                  name='landlineNo' onChange={handleInput} className='w-5/6'/></div>
                 </div>
 
               </div>
 
               <div className='flex flex-wrap justify-end'>
-                <Link to="/academic" className='bg-green-500 text-white p-1 rounded-md'>
+                <button type='submit' className='bg-green-500 text-white p-1 rounded-md'>
                   Save & Next
-                </Link>
+                </button>
               </div>
+              </form>
           </div>  
           
+          
         </div>
+        
           
         </div>
       
